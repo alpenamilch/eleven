6b0c44c3ae614fc94eea6eca2e9b2a4993680155
alpenamilch <alpenamilch@example.com>
2023-04-06 08:44:52 +0000

    initial

---
+++ Android.mk
@@ -0,0 +1,39 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_SRC_FILES := src/org/lineageos/eleven/IElevenService.aidl
+LOCAL_SRC_FILES += $(call all-java-files-under, src)
+LOCAL_SRC_FILES += $(call all-java-files-under, src_aosp)
+
+LOCAL_RESOURCE_DIR := $(addprefix $(LOCAL_PATH)/, res)
+
+LOCAL_STATIC_ANDROID_LIBRARIES := \
+    androidx.appcompat_appcompat \
+    androidx.cardview_cardview \
+    androidx-constraintlayout_constraintlayout \
+    androidx.core_core \
+    androidx.legacy_legacy-support-v4 \
+    androidx.palette_palette \
+    androidx.preference_preference \
+    androidx.recyclerview_recyclerview
+
+LOCAL_USE_AAPT2 := true
+
+LOCAL_PACKAGE_NAME := Eleven
+LOCAL_OVERRIDES_PACKAGES := Music
+
+LOCAL_PRIVATE_PLATFORM_APIS := true
+
+LOCAL_PRIVILEGED_MODULE := true
+
+LOCAL_JNI_SHARED_LIBRARIES := librsjni
+
+LOCAL_PROGUARD_FLAGS := -include $(LOCAL_PATH)/proguard.cfg
+LOCAL_PROGUARD_ENABLED := disabled
+
+include $(BUILD_PACKAGE)
+
+include $(CLEAR_VARS)
+include $(BUILD_MULTI_PREBUILT)
---
+++ README.md
@@ -0,0 +1,17 @@
+# LineageOS Eleven Material Theme / App Port
+ Material Theme for LineageOS Eleven (Designed for Pure Android / LineageOS Devices)
+ This app works from Android 8.x Oreo to Android 11 R so far
+ 
+ Note: This app respects Dark Theme on Android 10 Queen Cake!
+ 
+ Current Available Accent Colors: Pixel Blue, Eleven Pink, Android Green, Deep Purple
+ 
+ Planned Accent Colors: Coral Red, Banana Yellow, Cyan
+
+ Special Versions:
+ 
+ Heavily skinned Devices that run Samsung's OneUI / Touchwiz, Xiaomi's MIUI, Huawei's EMUI, etc...: Coming Soon!
+ 
+ LineageOS 15.1+ Devices / Custom ROMS that have Eleven Pre-Installed (14.1 and below does not apply): Coming Soon!
+
+ Screeenshots: Soon...
